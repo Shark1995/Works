632e78d6b19da17805d5dda302ab90109447bb85
Shark1995 <vasiljevaa@mail.ru>
2014-05-14 19:49:11 +0400

    все

---
+++ SinCos/SinCos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+namespace SinCos
+{
+    class Program
+    { 
+        static void Main()
+        {
+           double x = double.Parse(Console.ReadLine());
+           double y = double.Parse(Console.ReadLine());
+           double z;
+
+           z = x + y;
+           z = Math.Tan(z);
+           x = Math.Sin(x);
+           y = Math.Cos(y);
+
+           Console.WriteLine();
+           Console.WriteLine(x);
+           Console.WriteLine(y);
+           Console.WriteLine(z);
+           Console.ReadKey();
+        }
+    }
+}
